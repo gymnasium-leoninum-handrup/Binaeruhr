700bfc225bc1ad67a36fdfa3035d6765ab76dd72
EneOli <oliverenes01@gmail.com>
2018-03-04 13:32:36 +0100

    Neue Version der Rotary Klasse --> github.com/EneOli/SimpleArduinoRotaryEncoder

--- Rotary.hpp
+++ Rotary.hpp
@@ -8,59 +8,76 @@
 class Rotary
 {
 private:
-  int aLastState = 0;
-  int aState = 0;
-  int keyButton;
-  int key1;
-  int key2;
+    int aLastState = 0;
+    int aState = 0;
+    int keyButton;
+    int key1;
+    int key2;
 
 public:
-  Rotary(int keyButton, int key1, int key2)
-  {
-    this->keyButton = keyButton;
-    this->key1 = key1;
-    this->key2 = key2;
+    Rotary(int keyButton, int key1, int key2)
+    {
+        this->keyButton = keyButton;
+        this->key1 = key1;
+        this->key2 = key2;
 
-    pinMode(keyButton, INPUT);
-    pinMode(key1, INPUT);
-    pinMode(key2, INPUT);
+        pinMode(keyButton, INPUT);
+        pinMode(key1, INPUT);
+        pinMode(key2, INPUT);
 
-    digitalWrite(keyButton, HIGH);
-    digitalWrite(key1, HIGH);
-    digitalWrite(key2, HIGH);
+        digitalWrite(keyButton, HIGH);
+        digitalWrite(key1, HIGH);
+        digitalWrite(key2, HIGH);
 
-    aLastState = digitalRead(this->key1);
-  }
-
-  bool buttonPressed()
-  {
-    if (digitalRead(keyButton) == LOW)
-      return true;
-    else
-      return false;
-  }
-  int getTurn()
-  {
-    int result = 0;
-    aState = digitalRead(key1); // Reads the "current" state of the outputA
-    // If the previous and the current state of the outputA are different, that
-    // means a Pulse has occured
-    if (aState != aLastState && aState == LOW) {
-      // If the outputB state is different to the outputA state, that means the
-      // encoder is rotating clockwise
-      if (digitalRead(key2) != aState) {
-        result = 1;
-      } else {
-        result = 2;
-      }
-    } else {
-      result = 0;
+        aLastState = digitalRead(this->key1);
     }
-    aLastState = aState; // Updates the previous state of the outputA with the
-                         // current state
 
-    return result;
-  }
+    bool buttonPressed()
+    {
+        if (digitalRead(keyButton) == LOW)
+            return true;
+        else
+            return false;
+    }
+    int getTurn()
+    {
+        int result = 0;
+        aState = digitalRead(key1); // Reads the "current" state of the outputA
+        // If the previous and the current state of the outputA are different, that
+        // means a Pulse has occured
+        if (aState != aLastState && aState == LOW)
+        {
+            // If the outputB state is different to the outputA state, that means the
+            // encoder is rotating clockwise
+            if (digitalRead(key2) != aState)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 2;
+            }
+        }
+        else
+        {
+            result = 0;
+        }
+        aLastState = aState; // Updates the previous state of the outputA with the
+        // current state
+
+        return result;
+    }
+
+    int keyTime()
+    {
+        int count = 0;
+        while (buttonPressed())
+        {
+            count++;
+            delay(1);
+        }
+        return count;
+    }
 };
 
 #endif
