696a61eb648840fff4b8ab12921e5e72a6ccb969
Oliver Enes <oliver@enesnet.de>
2018-12-02 15:33:06 +0100

    Binaeruhr-neu.ino gelöscht

---
+++ Configuration.h
@@ -0,0 +1,27 @@
+//WLAN and NTP Server
+
+#define WLAN "MYWLAN"
+#define WLAN_PASSWORD "password42"
+#define NTP_SERVER "0.de.pool.ntp.org"
+
+//ESP Timeout (milliseconds)
+#define ESP_TIMEOUT 1000
+
+//Rotary directions
+#define RIGHT 1
+#define LEFT -1
+
+//Shift register digital
+#define LATCH_PIN 10
+#define CLOCK_PIN 9
+#define DATA_PIN 8
+
+// Schift register binary
+#define DATA_BIN 11
+#define CLOCK_BIN 12
+#define LATCH_BIN 13
+
+//Rotary pins
+#define ROTARY_KEY 7
+#define ROTARY_S2 6
+#define ROTARY_S1 5
